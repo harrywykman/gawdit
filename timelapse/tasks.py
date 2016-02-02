04b22ab9345365cf8726dff64508997a86f59789
Harry Wykman <harry@blackearthcollective.org.au>
2016-02-02 18:03:39 -0500

    changed to use rpi camera

--- timelapse/tasks.py
+++ timelapse/tasks.py
@@ -91,11 +91,11 @@ def rpi_capture_image(project, number):
         camera.exposure_compensation = 2
         camera.exposure_mode = 'spotlight'
         camera.meter_mode = 'matrix'
-            camera.image_effect = 'gpen'
-            # Give the camera some time to adjust to conditions
-            time.sleep(2)
-            camera.capture("%s%s" % (path, filename))
-            camera.stop_preview()
+        camera.image_effect = 'gpen'
+        # Give the camera some time to adjust to conditions
+        time.sleep(2)
+        camera.capture("%s%s" % (path, filename))
+        camera.stop_preview()
     image = File(open('%s%s' % (path, filename), 'r'))
     ti = TimelapseImage(name=name, image=image, timelapse=project)
     ti.save()
